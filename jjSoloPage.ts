d54f62f592b5c97eed876503c12b3908ecc51299
JJalessi <jjalessi.official@gmail.com>
2022-09-16 12:42:47 -0700

    Test Completed

--- jjSoloPage.ts
+++ jjSoloPage.ts
@@ -1,54 +1,52 @@
-import {BasePage} from './basePage'
-    import {By} from 'selenium-webdriver' 
+import {BasePage} from "./basePage"
+import {By} from 'selenium-webdriver'
 
-    export class SoloPage extends BasePage {
-        //Add/Remove Page
-        addRemoveLink: By = By.xpath('//a[@href="/add_remove_elements/"]')
-        addButton: By = By.css('[onclick="addElement()"]') 
-        deleteButton0: By = By.xpath('//button[text()="Delete"][0]')
-        deleteButton1: By = By.xpath('//button[text()="Delete"][1]') 
-        deleteButton2: By = By.xpath('//button[text()="Delete"][2]') 
+export class interWebs extends BasePage {
+      //Add/Remove Page
+      addRemoveLink: By = By.xpath('//a[@href="/add_remove_elements/"]')
+      addButton: By = By.css('[onclick="addElement()"]') 
+      deleteButton0: By = By.xpath('//button[text()="Delete"][0]')
+      deleteButton1: By = By.xpath('//button[text()="Delete"][1]') 
+      deleteButton2: By = By.xpath('//button[text()="Delete"][2]') 
 
-        //Dropdown Page
-        dropdownLink: By = By.xpath('//a[@href="/dropdown"]')
-        dropdownBar: By = By.xpath('//select[@id="dropdown"]')
-        option1: By = By.xpath('//option[@value="1"]') 
-        option2: By = By.xpath('//option[@value="2"]')
+      //Dropdown Page
+      dropdownLink: By = By.xpath('//a[@href="/dropdown"]')
+      dropdownBar: By = By.xpath('//select[@id="dropdown"]')
+      option1: By = By.xpath('//option[@value="1"]') 
+      option2: By = By.xpath('//option[@value="2"]')
 
-        //Drag and Drop Page 
-        dragDropLink: By = By.xpath('//a[@href="/drag_and_drop"]')
-        boxA: By = By.css('[id="column-a"]')
-        boxB: By = By.css('[id="column-b"]') 
+      //Drag and Drop Page 
+      dragDropLink: By = By.xpath('//a[@href="/drag_and_drop"]')
+      boxA: By = By.css('#column-a')
+      boxB: By = By.css('#column-b') 
 
-        //Inputs Page 
-        inputLink: By = By.xpath('//a[@href="/inputs"]')
-        numberInput: By = By.xpath('//input[@type="number"]')
+      //Inputs Page 
+      inputLink: By = By.xpath('//a[@href="/inputs"]')
+      numberInput: By = By.xpath('//input[@*]')
 
-        //Hovers Page
-        hoverLink: By = By.xpath('//a[@href="/hovers"]')
-        user1: By = By.xpath('//img[@src="/img/avatar-blank.jpg"][0]')
-        nameBox: By = By.xpath('//h5[text()="name: user1"]')
-        viewProfile: By = By.xpath('//a[@href="/users/1"]')
+      //Hovers Page
+      hoverLink: By = By.xpath('//a[@href="/hovers"]')
+      user1: By = By.xpath('(//div[@class="figure"])[1]')
+      nameBox: By = By.xpath('//h5[text()="name: user1"]')
+      viewProfile: By = By.xpath('//a[@href="/users/1"]')
 
-        //Context Menu Page 
-        menuPage: By = By.xpath('//a[@href="/context_menu"]')
-        contextBox: By = By.xpath('//div[@id="hot-spot"]')
-        contextMenuText: By = By.xpath('//script[text()="You selected a context menu"]')  
+      //Context Menu Page 
+      menuPage: By = By.xpath('//a[@href="/context_menu"]')
+      contextBox: By = By.xpath('//div[@id="hot-spot"]')
+      contextMenuText: By = By.xpath('//script[text()="You selected a context menu"]')  
 
-        //File Upload 
-        filePage: By = By.xpath('//a[@href="/upload"]')
-        chooseFile: By = By.xpath('//input[@id="file-upload"]')  
-        upload: By = By.css('[id="file-submit"]') 
-        successBox: By = By.css('[class="dz-success-mark dz-clickable"]')  
+      //File Upload 
+      filePage: By = By.xpath('//a[@href="/upload"]')
+      chooseFile: By = By.xpath('//input[@id="file-upload"]')  
+      upload: By = By.css('[id="file-submit"]') 
+      successBox: By = By.css('[class="dz-success-mark dz-clickable"]')  
 
-        //Geolocation Page
-        geoLink: By = By.xpath('//a[@href="/geolocation"]')
-        whereBtn: By = By.xpath('//button[@onclick="getLocation()"]')
-        googleLink: By = By.xpath('//a[@href="http://maps.google.com/?q=40.8389688,-115.7674698"]')
-        
-        constructor() {
-            super({url: 'https://the-internet.herokuapp.com/' })
-        }
-
-
+      //Geolocation Page
+      geoLink: By = By.xpath('//a[@href="/geolocation"]')
+      whereBtn: By = By.xpath('//button[text()="Where am I?"]')
+      googleLink: By = By.xpath('//a[@href="http://maps.google.com/?q=40.8389688,-115.7674698"]')
+      
+      constructor() {
+          super({url: "https://the-internet.herokuapp.com/"})
+      }
 }
--- jjSoloTest.test.ts
+++ jjSoloTest.test.ts
@@ -1,81 +1,50 @@
-import { WebDriver, WebElement } from "selenium-webdriver";
-import { Keyboard } from "selenium-webdriver/lib/input";
-import { elementIsNotVisible, elementIsVisible } from "selenium-webdriver/lib/until";
-import {SoloPage} from "./jjSoloPage";
-const page = new SoloPage()
-const fs = require ('fs') 
+import { buildPath } from 'selenium-webdriver/http'
+import {interWebs} from './jjSoloPage'
 
+const page = new interWebs() 
 
-describe("the-internet test", () => {
-    beforeEach(async () => {
-        await page.navigate();
-    })
-    afterAll(async () => {
-        await page.driver.sleep(7000)
-        await page.driver.quit()
-    }) 
-
-    test("Add/Remove Page", async () => {
-        await page.getElement(page.addRemoveLink)
-        await page.click(page.addRemoveLink)
-        await page.click(page.addButton)
-        await page.click(page.addButton)
-        await page.click(page.addButton) 
-        await page.click(page.deleteButton2)
-        //let deleteResults = await page.getElement(page.deleteButton2)
-        //expect(page.deleteButton2).toBe(elementIsNotVisible)
-    }) 
-    test("Dropdown Page", async () => {
-        await page.getElement(page.dropdownLink) 
-        await page.click(page.dropdownLink) 
-        await page.click(page.dropdownBar)
-        await page.click(page.option1)
-        await page.click(page.dropdownBar)
-        await page.click(page.option2) 
-        //await page.getElement(page.dropdownBar)
-        //    expect(page.dropdownBar).toContain("Option2") 
-    }) 
-    test("Inputs Page", async () => {
-        await page.getElement(page.inputLink)
-        await page.click(page.inputLink)
-        await page.getElement(page.numberInput)
-        await page.setInput(page.numberInput, '135') 
-        //How to sendKeys an up or down arrow click? 
-    }) 
-    test("Hovers Page", async () => {
-        await page.click(page.hoverLink)
-        await page.getElement(page.user1)
-        //is the user1 selector wrong? how to hover? 
-        await page.getElement(page.nameBox)
-        await page.click(page.viewProfile)
-        //should maybe fail profile page doesn't exist
-    })
-    test("Geolocation Page", async () => {
-        await page.getElement(page.geoLink)
-        await page.click(page.geoLink)
-        await page.click(page.whereBtn)
-        await page.click(page.googleLink)
-        //screenshot
-    })
-    test("Drag and Drop Page", async () => {
-        await page.getElement(page.dragDropLink)
-        await page.click(page.dragDropLink)
-        await page.getElement(page.boxA)
-        await page.getElement(page.boxB)
-        //don't know how to click and hold an element (and or) drag and drop
-    }) 
-    test("Context Menu Page", async () => {
-        await page.getElement(page.menuPage)
-        await page.click(page.menuPage)
-        await page.getElement(page.contextBox)
-        //how do you right click? also how do you verify a context menu, screenshot or getText? can you automate clicking the button in it?
-    }) 
-    test("File Upload Page", async () => {
-        await page.getElement(page.filePage)
-        await page.click(page.filePage)
-        await page.click(page.chooseFile)
-        //is the chooseFile selector wrong or is it even possible for selenium to recognize the file manager? 
-        //can you actually automate something like this? 
-    })
-    //how do you automate the mouse exiting the viewport pane?
+test("internet test ", async () => {
+  await page.navigate()
+  await page.getElement(page.addRemoveLink)
+  await page.click(page.addRemoveLink)
+  await page.click(page.addButton)
+  await page.click(page.addButton)
+  await page.click(page.addButton) 
+  await page.click(page.deleteButton2)
+  let deleteResults = await page.getElement(page.deleteButton2)
+  expect(deleteResults).not.toContain("results")
+ 
+})
+test("Dropdown Page", async () => {
+    await page.navigate()
+    await page.getElement(page.dropdownLink) 
+    await page.click(page.dropdownLink) 
+    await page.click(page.dropdownBar)
+    await page.click(page.option1)
+    await page.click(page.dropdownBar)
+    await page.click(page.option2) 
+    let dropdown = await page.getText(page.dropdownBar)
+    expect(dropdown).not.toContain("Please select an option Option 1 Option 2") 
+}) 
+test("Inputs Page", async () => {
+    await page.navigate()
+    await page.getElement(page.inputLink)
+    await page.click(page.inputLink)
+    await page.getElement(page.numberInput)
+    await page.setInput(page.numberInput, '135') 
+    //await page.setInput(page.numberInput, `\uE012`)
+}) 
+test("Drag and Drop Page", async () => {
+    await page.navigate()
+    await page.getElement(page.dragDropLink)
+    await page.click(page.dragDropLink)
+    await page.getElement(page.boxA)
+    await page.getElement(page.boxB)
+}) 
+test("Context Menu Page", async () => {
+    await page.navigate()
+    await page.getElement(page.menuPage)
+    await page.click(page.menuPage)
+    await page.getElement(page.contextBox)
+    await page.driver.quit()
 }) 
